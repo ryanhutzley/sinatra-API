9937251417c0ce7abe53ef5b6778f0cb89e9da0a
Ryan Hutzley <ryanhutzley@gmail.com>
2021-09-21 15:57:25 -0400

    change to database.yml and environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -13,7 +13,7 @@ configure :development do
 end
 
 configure :production do
-  db = URI.parse(ENV['postgres://absdxzqoyihdfn:9ea381314b0489246c55c87de5dd860d382899c67f6959ef2587e5ef4d652313@ec2-3-89-0-52.compute-1.amazonaws.com:5432/dfrneesl2i7opi'] || 'postgres://localhost/mydb')
+  db = URI.parse(ENV['postgresql-infinite-87698'] || 'postgres://localhost/mydb')
 
   ActiveRecord::Base.establish_connection(
     :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
